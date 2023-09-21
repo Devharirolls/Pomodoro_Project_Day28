812a908a15aee7eca9092b30d75e0f635e14020d
sanjay321654 <sanjayilaya2002@gmail.com>
2023-09-21 11:05:47 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import math
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
+BLUE = "#1ed1b1"
+G_Y = "#97d11e"
 YELLOW = "#f7f5dd"
+CARDINAL = "#D11e3e"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -18,7 +21,7 @@ timer = None
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
-    heading_label.config(text="Timer")
+    heading_label.config(text="Timer", fg="white")
     check_label.config(text="")
     global reps
     reps = 0
@@ -35,11 +38,11 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
-        heading_label.config(text="Long Break", fg=RED, font=(FONT_NAME, 20, "bold"))
+        heading_label.config(text="Long Break", fg=BLUE, font=(FONT_NAME, 20, "bold"))
 
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        heading_label.config(text="Short Break", fg=PINK, font=(FONT_NAME, 20, "bold"))
+        heading_label.config(text="Short Break", fg=G_Y, font=(FONT_NAME, 20, "bold"))
     else:
         count_down(work_sec)
         heading_label.config(text="Work", fg=GREEN, font=(FONT_NAME, 35, "bold"))
@@ -70,15 +73,15 @@ def count_down(count):
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, background=YELLOW)
+window.config(padx=100, pady=50, background=CARDINAL)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=CARDINAL, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=2, row=1)
 
-heading_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
+heading_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg="white", bg=CARDINAL)
 heading_label.grid(column=2, row=0)
 
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
@@ -87,7 +90,7 @@ start_button.grid(column=1, row=2)
 reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=3, row=2)
 
-check_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, "bold"))
+check_label = Label(fg=GREEN, bg=CARDINAL, font=(FONT_NAME, 16, "bold"))
 check_label.grid(column=2, row=3)
 
 window.mainloop()
